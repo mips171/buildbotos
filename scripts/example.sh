a4d7065fb722c1875e12a93ea76aa13b18832fe2
Hengjun Bei <hengjunbei@Hengjuns-MacBook-Pro.local>
2023-04-30 14:43:57 +1000

    init

--- scripts/example.sh
+++ scripts/example.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 echo 'This is an example shell script'
 echo 'Scripts here will run during build if specified in recipe.yml'
+
+# Setup Podman to emulator Docker
+systemctl --user enable --now podman.socket
+sudo ln -s /run/user/1000/podman/podman.sock /var/run/docker.sock
