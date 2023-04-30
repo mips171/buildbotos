d3ee69034c8901abc6a2baeae03382af3edfee38
Hengjun Bei <hengjunbei@Hengjuns-MacBook-Pro.local>
2023-04-30 14:52:38 +1000

    setup using just

--- scripts/example.sh
+++ scripts/example.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 echo 'This is an example shell script'
 echo 'Scripts here will run during build if specified in recipe.yml'
-
-# Setup Podman to emulator Docker
-systemctl --user enable --now podman.socket
-sudo ln -s /run/user/1000/podman/podman.sock /var/run/docker.sock
